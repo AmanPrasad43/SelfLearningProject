13fd7da5fea82c04fb99bcc8ab790e0fab9a4f8c
AMAN PRASAD <44187990+AmanPrasad43@users.noreply.github.com>
2023-10-07 14:06:51 +0530

    Items related to string code

---
+++ InitialPrograms/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            Console.Write("Please enter your name and press enter :");
+            myName = Console.ReadLine();
+            string myValue = String.Format("Upper case:{0}", myName.ToUpper());
+            Console.Write(myValue);
+        }
+    }
+}
